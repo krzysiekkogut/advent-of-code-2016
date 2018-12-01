e50129c61e67d5631448c3ec41da9323c491e241
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2018-12-01 13:15:48 +0100

    Day1 solution

--- src/solvers/1/Solver1.ts
+++ src/solvers/1/Solver1.ts
@@ -1,14 +1,30 @@
+import PuzzleVariant from '../../PuzzleVariant';
 import BaseSolver from '../BaseSolver';
 
-class Solver1 extends BaseSolver<string[]> {
+class Solver1 extends BaseSolver<number[]> {
   protected filePath: string = '1.txt';
 
-  protected solveInternal(input: string[]): string {
-    return input.join(' ');
+  protected solveInternal(input: number[]): string {
+    if (this.variant === PuzzleVariant.PART_1) {
+      return input.reduce((prev, curr) => prev + curr, 0).toString();
+    }
+
+    let i = 0;
+    let currentFreq = 0;
+    const frequencies = new Map<number, boolean>([[currentFreq, true]]);
+    while (true) {
+      currentFreq += input[i];
+      if (frequencies.get(currentFreq)) {
+        return currentFreq.toString();
+      }
+
+      frequencies.set(currentFreq, true);
+      i = (i + 1) % input.length;
+    }
   }
 
-  protected parseInput(textInput: string): string[] {
-    return textInput.split(',');
+  protected parseInput(textInput: string): number[] {
+    return textInput.split('\n').map(x => parseInt(x, 10));
   }
 }
 
