d5e5f32993110e89e0f7ae47ff2f7d9c5ab556ed
Krzysztof Kogut <krzysztof.kogut@altimi.com>
2018-12-11 11:05:50 +0100

    Inefficient solution part 2

--- src/solvers/Solver11.ts
+++ src/solvers/Solver11.ts
@@ -3,15 +3,15 @@ import BaseSolver from './BaseSolver';
 export default class Solver11 extends BaseSolver<number> {
   protected filePath: string = '11.txt';
 
-  private SIZE = 300;
+  private GRID_SIZE = 300;
 
   protected solvePart1(input: number): string {
     const grid = this.createGrid(input);
     let maxPower = -Infinity;
     let maxPowerCoords = { y: 0, x: 0 };
     const SQUARE_SIZE = 3;
-    for (let y = 1; y <= this.SIZE - SQUARE_SIZE + 1; y++) {
-      for (let x = 1; x < this.SIZE - SQUARE_SIZE + 1; x++) {
+    for (let y = 1; y <= this.GRID_SIZE - SQUARE_SIZE + 1; y++) {
+      for (let x = 1; x < this.GRID_SIZE - SQUARE_SIZE + 1; x++) {
         const power = this.calculatePower(grid, y, x, SQUARE_SIZE);
         if (power > maxPower) {
           maxPower = power;
@@ -24,7 +24,22 @@ export default class Solver11 extends BaseSolver<number> {
   }
 
   protected solvePart2(input: number): string {
-    throw new Error('Method not implemented.');
+    const grid = this.createGrid(input);
+    let maxPower = -Infinity;
+    let maxPowerCoords = { y: 0, x: 0, size: 0 };
+    for (let size = 1; size <= this.GRID_SIZE; size++) {
+      for (let y = 1; y <= this.GRID_SIZE - size + 1; y++) {
+        for (let x = 1; x < this.GRID_SIZE - size + 1; x++) {
+          const power = this.calculatePower(grid, y, x, size);
+          if (power > maxPower) {
+            maxPower = power;
+            maxPowerCoords = { y, x, size };
+          }
+        }
+      }
+    }
+
+    return `${maxPowerCoords.x},${maxPowerCoords.y},${maxPowerCoords.size}`;
   }
 
   protected parseInput(textInput: string): number {
@@ -32,13 +47,13 @@ export default class Solver11 extends BaseSolver<number> {
   }
 
   private createGrid(serialNumber: number): number[][] {
-    const grid: number[][] = new Array<number[]>(this.SIZE + 1);
+    const grid: number[][] = new Array<number[]>(this.GRID_SIZE + 1);
     for (let i = 0; i < grid.length; i++) {
-      grid[i] = new Array<number>(this.SIZE + 1);
+      grid[i] = new Array<number>(this.GRID_SIZE + 1);
     }
 
-    for (let y = 1; y <= this.SIZE; y++) {
-      for (let x = 1; x <= this.SIZE; x++) {
+    for (let y = 1; y <= this.GRID_SIZE; y++) {
+      for (let x = 1; x <= this.GRID_SIZE; x++) {
         const rackId = x + 10;
         let fuel = rackId * y;
         fuel += serialNumber;
