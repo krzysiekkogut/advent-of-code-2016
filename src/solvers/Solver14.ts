1096ac8964b1e0ce30005e8b5f5031aeedcb6bc8
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2018-12-23 20:09:52 +0100

    Day 14 part 2

--- src/solvers/Solver14.ts
+++ src/solvers/Solver14.ts
@@ -22,11 +22,39 @@ export default class Solver14 extends BaseSolver<number> {
       elf2 %= recipes.length;
     }
 
+    // tslint:disable-next-line:no-console
+    console.log(recipes.join(''));
+
     return recipes.slice(input, input + 10).join('');
   }
 
   protected solvePart2(input: number): string {
-    throw new Error('Method not implemented.');
+    const recipes: number[] = [3, 7];
+    let elf1 = 0;
+    let elf2 = 1;
+
+    let last11 = recipes.slice(-12).join('');
+
+    while (last11.indexOf(input.toString()) < 0) {
+      const newRecipe = recipes[elf1] + recipes[elf2];
+      if (newRecipe < 10) {
+        recipes.push(newRecipe);
+      } else {
+        recipes.push(Math.floor(newRecipe / 10), newRecipe % 10);
+      }
+
+      elf1 += recipes[elf1] + 1;
+      elf1 %= recipes.length;
+      elf2 += recipes[elf2] + 1;
+      elf2 %= recipes.length;
+
+      last11 = recipes.slice(-11).join('');
+    }
+
+    return recipes
+      .join('')
+      .indexOf(input.toString())
+      .toString();
   }
 
   protected parseInput(textInput: string): number {
