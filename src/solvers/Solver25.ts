1a69bb917fedb6e47b90a9cd55a2b1fe478b71f3
Krzysztof Kogut <krzysztof.kogut@altimi.com>
2019-01-02 12:01:28 +0100

    Day 25 solution

--- src/solvers/Solver25.ts
+++ src/solvers/Solver25.ts
@@ -12,11 +12,32 @@ export default class Solver25 extends BaseSolver<ISpaceTimePoint[]> {
   protected filePath = '25.txt';
 
   protected solvePart1(input: ISpaceTimePoint[]): string {
-    throw new Error('Method not implemented.');
+    let constellations: ISpaceTimePoint[][] = [];
+    for (const point of input) {
+      const newConstellations: ISpaceTimePoint[][] = [];
+      const matchingConstellations: ISpaceTimePoint[][] = [];
+      for (const constellation of constellations) {
+        if (constellation.some(constellationPoint => this.getDistance(constellationPoint, point) <= 3)) {
+          matchingConstellations.push(constellation);
+        } else {
+          newConstellations.push(constellation);
+        }
+      }
+
+      if (matchingConstellations.length === 0) {
+        newConstellations.push([point]);
+      } else {
+        newConstellations.push([point].concat(...matchingConstellations));
+      }
+
+      constellations = newConstellations;
+    }
+
+    return constellations.length.toString();
   }
 
   protected solvePart2(input: ISpaceTimePoint[]): string {
-    throw new Error('Method not implemented.');
+    return 'Day 25 does not have second part :)';
   }
 
   protected parseInput(textInput: string): ISpaceTimePoint[] {
@@ -31,8 +52,17 @@ export default class Solver25 extends BaseSolver<ISpaceTimePoint[]> {
           y: parseInt(coords[1], 10),
           z: parseInt(coords[2], 10),
           // tslint:disable-next-line:object-literal-sort-keys
-          t: parseInt(coords[4], 10),
+          t: parseInt(coords[3], 10),
         } as ISpaceTimePoint;
       });
+  }
+
+  private getDistance(pointA: ISpaceTimePoint, pointB: ISpaceTimePoint): number {
+    const xDistance = Math.abs(pointA.x - pointB.x);
+    const yDistance = Math.abs(pointA.y - pointB.y);
+    const zDistance = Math.abs(pointA.z - pointB.z);
+    const tDistance = Math.abs(pointA.t - pointB.t);
+
+    return xDistance + yDistance + zDistance + tDistance;
   }
 }
